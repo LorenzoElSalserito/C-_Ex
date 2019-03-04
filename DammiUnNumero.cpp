c2e58414afb7d2a493aa12fa779f27c657c53e8e
Lorenzo De Marco <35295037+LorenzoElSalserito@users.noreply.github.com>
2019-03-04 17:59:18 +0100

    Create DammiUnNumero.cpp

---
+++ DammiUnNumero.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (){
+int numero;
+std::cout<<"Inserisci il numero che vorresti vedere sul video:";
+std::cin>>numero;
+std::cout<<"Il numero che hai inserito è -> "<< numero <<" visto che bello?"<<std::endl;
+return 0;
+}
