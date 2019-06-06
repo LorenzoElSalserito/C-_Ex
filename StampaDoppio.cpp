8e65d8950c3bef16656db018310d27b94eae7ca2
Lorenzo De Marco <35295037+LorenzoElSalserito@users.noreply.github.com>
2019-06-06 17:06:43 +0200

    Create StampaDoppio.cpp

---
+++ StampaDoppio.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <string>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+//questo programma prende in input un numero e ne stampa il doppio;
+
+int main (){
+int numero;
+int doppio;
+
+cout<<"Dammi il numero per cui vuoi che io ne stampi il doppio"<<endl;
+cin>>numero;
+doppio=numero*2;
+cout<<"Ecco il suo doppio "<<doppio<<endl;
+ return 0;
+}
