e1477167f6d4f2aeffff5862e1ca62e288f6d8c5
Lorenzo De Marco <35295037+LorenzoElSalserito@users.noreply.github.com>
2019-06-10 17:28:51 +0200

    Create ProvaFunzioniEMetodi.cpp

---
+++ ProvaFunzioniEMetodi.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <string>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+//prova di scrittura di un programma con funzioni
+
+void StampaSaluto(){
+	cout<<"Ciao Bella Gente!"<<endl;
+	}
+
+int main (){
+	StampaSaluto();
+	return 0;
+}
