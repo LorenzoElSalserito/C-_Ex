30cfe4769211e7609d34194e8759a277a6e45e50
Lorenzo De Marco <35295037+LorenzoElSalserito@users.noreply.github.com>
2019-06-25 18:14:23 +0200

    Create StampaMAtriceCarattere.cpp

---
+++ StampaMAtriceCarattere.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <string>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+void GeneraMatrice(int **M, int righe, int colonne){
+    for(int k=0; k<righe; k++){
+		M[k] = new int[righe];
+		M[k] = new int[colonne];
+		}
+	for (int i=0;i<righe;i++){
+		for(int j=0;j<colonne;j++){
+		M[i][j]=0;
+		}
+	  }
+	}
+
+void StampaMatrice(int **M, int righe, int colonne, char c){
+
+    for (int i=0; i<righe*righe; i++){
+		cout<<c;
+		}
+	for(int j=1; j<colonne*colonne; j++){
+		cout<<endl<<c;
+		}	
+}
+
+
+int main (){
+    int x=0;
+	char c='r';
+	int **M;
+	cout<<"Dammi la dimensione della matrice"<<endl;
+	cin>>x;
+	M = new int*[x];
+GeneraMatrice(M, x, x);
+StampaMatrice(M, x, x, c);
+return 0;
+}
+
